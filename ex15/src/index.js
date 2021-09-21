31048ec92fbd2b66a96d15de986b7ccc078f42cd
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-21 16:06:13 +0200

    Created folder 15

---
+++ ex15/src/index.js
@@ -0,0 +1,27 @@
+// Only change code below this line
+
+function myDoWhile() {
+
+var myNumbers = "";
+
+var i = 0;
+
+while (i < 10) {
+
+if (i === 9) {
+    myNumbers += i; 
+    } else {
+    myNumbers += i + "," + " ";
+}
+
+i++;
+}
+
+return myNumbers;
+}
+
+console.log(myDoWhile());
+
+// Only change code above this line
+
+module.exports = myDoWhile;
