742bdb45be49427af0f9735d504c4a6a899c1cb5
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-21 09:28:47 +0200

    Fixed

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -2,9 +2,9 @@
 
 function myFunction(myParameter) {
 if (myParameter) {
-    return "The parameter is true!"
+    return "The parameter is true!";
 }
-return "The parameter is false!"
+return "The parameter is false!";
 }
 
 console.log(myFunction(true));
